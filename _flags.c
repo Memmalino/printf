fc5b9ee371a32e9703bf4094669d01bce41f5a51
memmalino <memmalino@gmail.com>
2023-04-24 06:38:08 -0700

    latest

---
+++ _flags.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_flags - function that calculate working flags
+ * @format: the format with which the argument is printed
+ * @i: int type of an argument
+ * Return: Flags
+ */
+
+int get_flags(const char *format, int *i)
+{
+	int a = 0, b;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (b = *i + 1; format[b] != '\0'; b++)
+	{
+		for (; FLAGS_CH[a] != '\0'; a++)
+			if (format[b] == FLAGS_CH[a])
+			{
+				flags |= FLAGS_ARR[a];
+				break;
+			}
+
+		if (FLAGS_CH[a] == 0)
+			break;
+	}
+
+	*i = b - 1;
+
+	return (flags);
+}
+
